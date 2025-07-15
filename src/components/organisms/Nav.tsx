35f348b55e1eb7c03567c421186ae076b587c691
cfmcfmcfmcfm <conradmahnkopf@gmail.com>
2025-07-15 09:37:21 +0200

    modified nav

--- src/components/organisms/Nav.tsx
+++ src/components/organisms/Nav.tsx
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 const Nav = () => {
-  return <nav>placetel.ai</nav>;
+  return (
+    <nav className="fixed inset-x-0 top-0 p-2 z-50">
+      <div className="w-full rounded-xl overflow-hidden bg-blue-600 p-4 text-white flex justify-between items-center">
+        <ul className="flex gap-4 items-center">
+          <p>Placetel.ai</p>
+          <Link href="/ai-agents">
+            <li>Ai Agents</li>
+          </Link>
+          <Link href="/ai-pro">
+            <li>Ai Pro</li>
+          </Link>
+        </ul>
+
+        <ul className="flex gap-4 items-center">
+          <Button variant="secondary">Contact Sales</Button>
+          <li>
+            <Button>Start free trial</Button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/components/pages/AiPro.tsx
+++ src/components/pages/AiPro.tsx
@@ -1,9 +1,11 @@
 import CTASection from "@/components/molecules/CTASection";
 import SharedLayoutAnimation from "../molecules/TabTest";
+import RoiCalculator from "../molecules/RoiCalculator";
 
 const AiPro = () => {
   return (
     <div>
+      <RoiCalculator />
       <SharedLayoutAnimation />
       <CTASection />
     </div>

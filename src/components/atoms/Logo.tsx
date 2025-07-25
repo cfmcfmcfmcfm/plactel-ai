3a435afd93caff7ed4d01a9ee15d5303e1550139
cfmcfmcfmcfm <conradmahnkopf@gmail.com>
2025-07-25 13:09:17 +0200

    modified logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -2,21 +2,28 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
-// import { useTheme } from "next-themes";
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
 
 const Logo = () => {
-    // const { theme } = useTheme();
+    const { theme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) return null; // Or render a placeholder image
+
+    const logoSrc =
+        theme === 'dark'
+            ? '/assets/plt-gamma-logo-weiß.png'
+            : '/assets/plt-gamma-logo.png';
 
     return (
         <Link href="/" className="w-40 shrink-0 grow-0">
             <Image
-                // src={
-                //   theme === "dark"
-                //     ? "/assets/plt-gamma-logo-weiß.png"
-                //     : "/assets/plt-gamma-logo.png"
-                // }
-                src={'/assets/plt-gamma-logo-weiß.png'}
+                src={logoSrc}
                 width={1700}
                 height={390}
                 alt="Placetel Logo"
